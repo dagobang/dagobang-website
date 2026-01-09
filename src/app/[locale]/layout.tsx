36906d66203f581bd599fc3eff95ddd623fdfd24
SeasonsRich <0xseasonsrich@gmail.com>
2026-01-09 12:24:09 +0800

    updates

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { NextIntlClientProvider, useMessages } from "next-intl";
 
@@ -5,6 +6,51 @@ import { ThemeProvider } from '@/components/ThemeProvider';
 import { SUPPORTED_LOCALES } from "@/constants/locale";
 
 import "@/styles/globals.css";
+
+export async function generateMetadata(
+  { params }: { params: { locale: string } }
+): Promise<Metadata> {
+  const banner =
+    params.locale === "zh"
+      ? "/images/dgb-banner-zh.png"
+      : "/images/dgb-banner-en.png";
+
+  return {
+    title: {
+      template: "%s",
+      default: "Dagobang 打狗棒",
+    },
+    applicationName: "Dagobang 打狗棒",
+    description: "0.1% Fee Meme Trading Tool",
+    authors: {
+      name: "Dagobang",
+      url: "https://github.com/dagobang/dagobang-website",
+    },
+    icons: "/favicon.ico",
+    openGraph: {
+      type: "website",
+      url: "https://dagobang.vercel.app",
+      title: "Dagobang 打狗棒",
+      description: "0.1% Fee Meme Trading Tool",
+      siteName: "Dagobang 打狗棒",
+      images: [
+        {
+          url: banner,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "https://dagobang.vercel.app",
+      creator: "@SeasonsRichs",
+      title: {
+        template: "%s",
+        default: "Dagobang 打狗棒",
+      },
+      images: banner,
+    },
+  };
+}
 
 
 export default function LocaleLayout({
